6d28769135c58796e65292f084667fda3e7664ff
Raul Torres <raulunihamburg@googlemail.com>
2023-09-17 20:06:38 +0300

    [NFC] Use protok as namespace in example.cpp

--- example/example.cpp
+++ example/example.cpp
@@ -1,13 +1,15 @@
 #include <protok.h>
 
+using namespace protok;
+
 int main() {
   int N = 1000;
   int A[1000] = {1};
   int B[1000] = {2};
   int C[1000] = {0};
 
-  protok::compute({.BaseCU = protok::ComputingUnity::CPU,
-                   .DistributionCU = protok::ComputingUnity::THREAD},
-                  {.lowerbound = 0, .upperbound = N, .stride = 1},
-                  [&A, &B, &C](int &i) { C[i] = A[i] + B[i]; });
+  compute(
+      {.BaseCU = ComputingUnity::CPU, .DistributionCU = ComputingUnity::THREAD},
+      {.lowerbound = 0, .upperbound = N, .stride = 1},
+      [&A, &B, &C](int &i) { C[i] = A[i] + B[i]; });
 }
