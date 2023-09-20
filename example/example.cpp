bad9fc7a93ab06cf78bb4a34259e29bb61a6a989
Raul Torres <raulunihamburg@googlemail.com>
2023-09-21 00:50:40 +0300

    Add support for matrices + create a class for distributions

--- example/example.cpp
+++ example/example.cpp
@@ -4,17 +4,26 @@ using namespace protok;
 
 int main() {
   int N = 1000;
-  int A[1000] = {1};
-  int B[1000] = {2};
-  int C[1000] = {0};
+  int A[1000];
+  int B[1000];
+  int C[1000];
 
-  compute(
-      {.BaseCU = ComputingUnity::CPU, .DistributionCU = ComputingUnity::THREAD},
-      {.lowerbound = 0, .upperbound = N, .stride = 1},
-      [&A, &B, &C](int &i) { C[i] = A[i] + B[i]; });
+  Space I = {.lowerbound = 0, .upperbound = N, .stride = 1};
 
-  compute(
-      {.BaseCU = ComputingUnity::ACCEL, .DistributionCU = ComputingUnity::THREAD},
-      {.lowerbound = 0, .upperbound = N, .stride = 1},
-      [&A, &B, &C](int &i) { A[i] = B[i] * C[i]; });
+  compute(Distributions::CpuOnThreads(), I,
+          [&A, &B, &C](int &i) { C[i] = A[i] + B[i]; });
+
+  compute(Distributions::AccelOnTeams(),
+          {.lowerbound = 0, .upperbound = N, .stride = 1},
+          [&A, &B, &C](int &i) { A[i] = B[i] * C[i]; });
+
+  int M = 100;
+  int D[100][100];
+  int E[100][100];
+  int F[100][100];
+
+  Space X, Y = {.lowerbound = 0, .upperbound = M, .stride = 1};
+
+  compute(Distributions::CpuOnThreads(), X, Y,
+          [&D, &E, &F](int &i, int &j) { F[i][j] = D[i][j] * E[i][j]; });
 }
--- include/protok.h
+++ include/protok.h
@@ -14,6 +14,18 @@ struct ComputingDistribution {
   ComputingUnity DistributionCU;
 };
 
+struct Distributions {
+  static ComputingDistribution CpuOnThreads() {
+    return {.BaseCU = ComputingUnity::CPU,
+            .DistributionCU = ComputingUnity::THREAD};
+  }
+
+  static ComputingDistribution AccelOnTeams() {
+    return {.BaseCU = ComputingUnity::ACCEL,
+            .DistributionCU = ComputingUnity::TEAM};
+  }
+};
+
 template <typename Kernel>
 int compute(ComputingDistribution distribution, Space space, Kernel kernel) {
   switch (distribution.BaseCU) {
@@ -44,6 +56,67 @@ int compute(ComputingDistribution distribution, Space space, Kernel kernel) {
 #pragma omp target teams distribute
       for (int i = space.lowerbound; i < space.upperbound; i += space.stride) {
         kernel(i);
+      }
+      break;
+    default:
+      std::cerr
+          << "The provided level of parallelism inside an accelerator is not "
+             "supported yet\n";
+      exit(0);
+    }
+    break;
+
+  default:
+    std::cerr << "The provided base computing unity is not supported yet\n";
+    exit(0);
+  }
+
+  return 0;
+}
+
+template <typename Kernel>
+int compute(ComputingDistribution distribution, Space outerspace,
+            Space innerspace, Kernel kernel) {
+  switch (distribution.BaseCU) {
+  case ComputingUnity::CPU:
+    switch (distribution.DistributionCU) {
+    case ComputingUnity::THREAD:
+#pragma omp parallel for collapse(2)
+      for (int i = outerspace.lowerbound; i < outerspace.upperbound;
+           i += outerspace.stride) {
+        for (int j = innerspace.lowerbound; j < innerspace.upperbound;
+             j += innerspace.stride) {
+          kernel(i, j);
+        }
+      }
+      break;
+    case ComputingUnity::VECTOR:
+#pragma omp simd collapse(2)
+      for (int i = outerspace.lowerbound; i < outerspace.upperbound;
+           i += outerspace.stride) {
+        for (int j = innerspace.lowerbound; j < innerspace.upperbound;
+             j += innerspace.stride) {
+          kernel(i, j);
+        }
+      }
+      break;
+    default:
+      std::cerr << "The provided level of parallelism inside a CPU is not "
+                   "supported yet\n";
+      exit(0);
+    }
+    break;
+
+  case ComputingUnity::ACCEL:
+    switch (distribution.DistributionCU) {
+    case ComputingUnity::TEAM:
+#pragma omp target teams distribute collapse(2)
+      for (int i = outerspace.lowerbound; i < outerspace.upperbound;
+           i += outerspace.stride) {
+        for (int j = innerspace.lowerbound; j < innerspace.upperbound;
+             j += innerspace.stride) {
+          kernel(i, j);
+        }
       }
       break;
     default:
