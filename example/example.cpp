4ca17506bec9b6797e9e3ec8a39e23688ff4174d
Raul Torres <raulunihamburg@googlemail.com>
2023-10-16 22:26:20 -0500

    Organize examples into separated functions

--- example/example.cpp
+++ example/example.cpp
@@ -2,9 +2,8 @@
 
 using namespace protok;
 
-int main() {
-  // one-to-one addition of vectors
-
+// one-to-one addition of vectors
+static void vector_add() {
   int N = 1000;
   int A[1000];
   int B[1000];
@@ -16,40 +15,57 @@ int main() {
       {.BaseCU = ComputingUnity::CPU, .DistributionCU = ComputingUnity::THREAD},
       {.lowerbound = 0, .upperbound = N, .stride = 1, .type = RangeType::SPACE},
       [&A, &B, &C](int &i) { C[i] = A[i] + B[i]; });
+}
 
-  // one-to-one multiplication of vectors
+// one-to-one multiplication of vectors
+static void vector_mul() {
+  int N = 1000;
+  int A[1000];
+  int B[1000];
+  int C[1000];
+
+  // data initialization goes here ...
 
   compute(Distributions::AccelOnTeams(), {0, N, 1},
           [&A, &B, &C](int &i) { A[i] = B[i] * C[i]; });
+}
 
-  // one-to-one multiplication of matrices
-
-  int M = 100;
-  int D[100][100];
-  int E[100][100];
-  int F[100][100];
+// one-to-one multiplication of matrices
+static void matrix_mul_naive() {
+  int N = 100;
+  int A[100][100];
+  int B[100][100];
+  int C[100][100];
 
   // data initialization goes here ...
 
-  Range X = {0, M, 1, RangeType::SPACE};
-  Range Y = {0, M, 1, RangeType::SPACE};
+  Range X = {0, N, 1, RangeType::SPACE};
+  Range Y = {0, N, 1, RangeType::SPACE};
   compute(Distributions::CpuOnThreads(), X, Y,
-          [&D, &E, &F](int &i, int &j) { F[i][j] = D[i][j] * E[i][j]; });
+          [&A, &B, &C](int &i, int &j) { C[i][j] = A[i][j] * B[i][j]; });
+}
 
-  // matrix multiplication
-
-  int L = 100;
-  int G[100][100];
-  int H[100][100];
-  int I[100][100];
+// matrix multiplication
+static void matrix_mul() {
+  int N = 100;
+  int A[100][100];
+  int B[100][100];
+  int C[100][100];
 
   // data initialization goes here ...
 
-  Range S = {0, L, 1, RangeType::SPACE};
-  Range T = {0, L, 1, RangeType::SPACE};
-  Range U = {0, L, 1, RangeType::DIMENSION};
+  Range X = {0, N, 1, RangeType::SPACE};
+  Range Y = {0, N, 1, RangeType::SPACE};
+  Range Z = {0, N, 1, RangeType::DIMENSION};
 
   compute(
-      Distributions::CpuOnThreads(), S, T, U,
-      [&G, &H, &I](int &i, int &j, int &k) { I[i][j] += G[i][k] * H[k][j]; });
+      Distributions::CpuOnThreads(), X, Y, Z,
+      [&A, &B, &C](int &i, int &j, int &k) { C[i][j] += A[i][k] * B[k][j]; });
+}
+
+int main() {
+  vector_add();
+  vector_mul();
+  matrix_mul_naive();
+  matrix_mul();
 }
